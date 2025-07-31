c0243dbb39a9b202444e2d00cfe6254624700877
ensaktrn <enesakturan2002@gmail.com>
2025-07-31 23:16:40 +0300

    token expiration time updated

--- utils/token.js
+++ utils/token.js
@@ -4,7 +4,7 @@ const ACCESS_SECRET = process.env.JWT_SECRET;
 const REFRESH_SECRET = process.env.REFRESH_SECRET || 'refreshsecret';
 
 exports.createAccessToken = (payload) => {
-  return jwt.sign(payload, ACCESS_SECRET, { expiresIn: '15m' });
+  return jwt.sign(payload, ACCESS_SECRET, { expiresIn: '1h' });
 };
 
 exports.createRefreshToken = (payload) => {
